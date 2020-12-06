b5db5b2b0954c2301d27e0a8a8b231b3801f6c29
ibro365 <ibrahimmoallim3@gmail.com>
2020-12-06 17:41:55 -0500

    completed majority of app logic

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'init',
+      isLoaded: false,
+      weatherData: null
+    }
+  }
+
+  abortController = new AbortController();
+  controllerSignal = this.abortController.signal;
+
+  
+
+  getWeatherData = () => {
+    const weatherApi = `api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+
+    fetch(weatherApi, { signal: this.controllerSignal })
+    .then(response => response.json())
+    .then(
+      (result) => {
+        console.log(result);
+        const { name } = result;
+        const { country } = result.sys;
+        const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
+        const { description, icon } = result.weather[0];
+        const { speed, deg } = result.wind;
+
+        this.setState({
+          isLoaded: true,
+          weatherData: {
+            name,
+            country,
+            description,
+            icon,
+            temp: temp.toFixed(1),
+            feels_like: feels_like.toFixed(1),
+            temp_min: temp_min.toFixed(1),
+            temp_max: temp_max.toFixed(1),
+            speed,
+            deg,
+            humidity
+          }
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.getWeatherData();
+  }
+
+  componentWillUnmount() {
+    this.abortController.abort();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='container'>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
